a9ffacb478cf8ac580e1b79fa98e53f8135cf2c9
flaviu <flaviumarinescu@gmail.com>
2021-11-27 15:58:27 +0200

    added docstrings & pylint recommended changes

--- scheduler/pipelines/__init__.py
+++ scheduler/pipelines/__init__.py
@@ -1,10 +1,22 @@
+"""Functions that are shared by multiple pipelines"""
+
 from typing import Any
+import pickle
 import pandas as pd
 import redis
-import pickle
 
 
 def load(obj: Any, key: str, cache: redis.Redis) -> bool:
+    """Stores any pickleable python  object inside a redis instance.
+
+    Args:
+        obj (Any): Any python object.
+        key (str): Key for telling redis where to store object.
+        cache (redis.Redis): Redis connection.
+
+    Returns:
+        bool:  True if successfully stored object, False otherwise.
+    """
     pickeld_ob = pickle.dumps(obj, protocol=pickle.HIGHEST_PROTOCOL)
     return cache.set(key, pickeld_ob)
 
@@ -12,9 +24,19 @@ def load(obj: Any, key: str, cache: redis.Redis) -> bool:
 def common_clean(
     df: pd.DataFrame,
     dropna: str = "any",
-    tz: str = "Europe/Bucharest",
+    timezone: str = "Europe/Bucharest",
 ) -> pd.DataFrame:
-    df = df.tz_convert(tz)
+    """Common data cleaning steps.
+
+    Args:
+        df (pd.DataFrame): Pandas dataframe with open high low close columns and datetime index
+        dropna (str, optional): Removes any Nan values from dataframe. Defaults to "any".
+        timezone (str, optional): Switches timezone to this values. Defaults to "Europe/Bucharest".
+
+    Returns:
+        pd.DataFrame: Returns processed dataframe.
+    """
+    df = df.tz_convert(timezone)
     df = df.tz_localize(None)
 
     if dropna:
